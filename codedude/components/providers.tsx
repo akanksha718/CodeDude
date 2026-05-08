f9edc77b4f1e64040787d5e5b71e7190255f6dc7
Akanksha negi <ankiakanksha707@gmail.com>
2026-05-08 18:37:30 +0530

    hi

--- codedude/components/providers.tsx
+++ codedude/components/providers.tsx
@@ -1,12 +1,30 @@
 "use client"
-function ClerkWithTheme({ children }: { children: React.ReactNode }) {
-    // const {} = useTheme()
 
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider, useTheme } from "./theme-provider"
+import {dark} from "@clerk/themes";
+
+function ClerkWithTheme({ children }: { children: React.ReactNode }) {
+    const {theme} = useTheme();
+    const isDark = theme==="dark" ||
+     (theme === "system" && 
+        (typeof window === "undefined" ||
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+        )
+     );
+     return (<ClerkProvider appearance={{
+        baseTheme: isDark ? dark : "undefined"
+     }}>
+        {children}
+     </ClerkProvider>)
 }
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {children}
-    </>
+      <ThemeProvider defaultTheme="dark">
+        <ClerkWithTheme>
+        {children}
+      </ClerkWithTheme>
+      </ThemeProvider>
+   
   )
 }
