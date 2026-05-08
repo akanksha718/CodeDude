0b460d808782bfe66cf943843c24dd846c82ea2b
Akanksha negi <ankiakanksha707@gmail.com>
2026-05-08 14:31:22 +0530

    hi

---
+++ codedude/components/providers.tsx
@@ -0,0 +1,12 @@
+"use client"
+function ClerkWithTheme({ children }: { children: React.ReactNode }) {
+    // const {} = useTheme()
+
+}
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
